597f9a355d50c53fb7f1abb86b6e62f1cb982ade
Suraj <suraj@evolveasia.co>
2024-01-25 22:41:27 +0545

    added complete code

--- src/wapPractice/FIbSeries.java
+++ src/wapPractice/FIbSeries.java
@@ -2,9 +2,29 @@ package wapPractice;
 
 public class FIbSeries {
 
+	
+	static int a= 0, b = 1, c;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.print(a+" "+b);
+		FIbSeries fb = new FIbSeries();
+		fb.printFiB(10);
+		
 
+	}
+	
+	
+	void printFiB(int n) {
+		
+		if(n>1) {
+			c = a+b;
+			System.out.print(" "+c);
+			a = b;
+			b=c;
+			printFiB(n-1);
+		}
+		
+		
 	}
 
 }
